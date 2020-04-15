2537df6de7e79dc6cbf7eaea7c3113ff9028cef8
SamoKopecky <sano.kopecky@gmail.com>
2020-04-15 19:05:03 +0200

    crated Makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+default: main.c
+	gcc -std=gnu99 -Wall -Wextra -Werror -pedantic -o proj2 main.c
--- main.c
+++ main.c
@@ -1,6 +1,12 @@
+#include <sys/mman.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
-   return 0;
+   printf("make works\n");
+   fork();
+   sleep(50000);
 }
