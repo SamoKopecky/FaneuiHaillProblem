b12d7191a8b7dc389cbb3d8085b12ea6ec97699b
SamoKopecky <sano.kopecky@gmail.com>
2020-04-18 17:54:15 +0200

    Revert "Revert "NOT WORKING""
    This reverts commit 2f5fcfedc729feec1e4f13ba6c146ea2b9e2f882.

--- immigrant_factory/immigrant_factory.h
+++ immigrant_factory/immigrant_factory.h
@@ -1,8 +1,8 @@
 #ifndef IMMIGRANT_FACTORY_H_
 #define IMMIGRANT_FACTORY_H_
 
-#include "immigrant.h"
-#include "includes.h"
+#include "../immigrant/immigrant.h"
+#include "../includes.h"
 #include <time.h>
 
 void immigrant_factory(int *PI_ptr, action_counter_sync_t action_counter_sync, immigrant_info_t immigrant_info, semaphores_t semaphores);
